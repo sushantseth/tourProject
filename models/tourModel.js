651154bbec18a2cc87b7faa8cea069d03fc0cc21
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-04-25 19:07:22 +0530

    Update tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -42,7 +42,8 @@ const tourSchema = new mongoose.Schema({
     },
     createdAt : {
         type : Date,
-        default : Date.now()
+        default : Date.now(),
+        select : false
     },
     startDates : [Date],
     description : {
