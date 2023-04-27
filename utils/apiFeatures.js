b07a67c24a4fbceb4a1985a190e1adea6178e77e
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-04-28 03:16:32 +0530

    Add files via upload

---
+++ utils/apiFeatures.js
@@ -0,0 +1,45 @@
+exports.queryFunction = (model, queryObj) => {
+
+    console.log(queryObj)
+    let finalqueryObj = {...queryObj}
+    const excludedFields = ["page","sort","limit","fields"]
+    excludedFields.forEach(el => delete finalqueryObj[el])
+    // console.log(queryObj)
+    const updatedQuery = JSON.parse(JSON.stringify(finalqueryObj).replace(/\b(gte|gt|lte|lt)\b/g, match => {
+        switch (match) {
+          case 'gte':
+            return '$gte';
+          case 'gt':
+            return '$gt';
+          case 'lte':
+            return '$lte';
+          case 'lt':
+            return '$lt';
+          default:
+            return match;
+        }
+      }));
+
+      let query = model.find(updatedQuery)
+
+      if(queryObj.sort){
+        const sortBy = queryObj.sort.split(",").join(" ")
+         query.sort(sortBy)
+      }
+
+      if(queryObj.fields){
+        const selectBy = queryObj.sort.split(",").join(" ")
+        query = query.select(selectBy)
+       }else{
+        query = query.select("-createdDate") 
+       }
+
+       if(queryObj.limit){
+        console.log("inside limit")
+       let limit = queryObj.limit ?  queryObj.limit : 3
+       let page = queryObj.page ? queryObj.page : 1 
+       let skipValue = ( page - 1 ) * 3 
+        query =  query.skip(skipValue).limit(limit)
+     }
+    return query
+}
