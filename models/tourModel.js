3f6f877c982b9e0e67caa1d1001362aedd9d4498
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-04-28 17:44:04 +0530

    Update tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-
+const Counter = require('../models/counterModel')
 
 //created schema
 const tourSchema = new mongoose.Schema({
@@ -49,9 +49,58 @@ const tourSchema = new mongoose.Schema({
     description : {
         type : String,
         trim : true
-    }
+    },
+    secretTour : Boolean
 
+},{
+    toJSON : {virtuals:true},
+    toObject : {virtuals:true}
 })
+
+tourSchema.virtual('shortSummary').get(function(){
+    return this.summary.slice(0,10)
+})
+
+//document middleware
+
+tourSchema.pre('save',function(next){
+    const Tour = mongoose.model('Tour',tourSchema)
+    Tour.findOne({name : this.name})
+    .then((data)=>{
+        if(data){
+            return next(new Error("tour already exists"))
+        }
+        next()
+    })
+    .catch(err => next(err));
+})
+
+tourSchema.post('save',function(doc,next){
+    Counter.findOneAndUpdate({name : "TourCounter"},{$inc : {count : 1}},{upsert : true, new : true})
+    .then((data)=> next())
+    .catch(err => next(err))
+})
+
+//query middleware
+tourSchema.pre(`/^find`,function(next){
+    //this object is the query object 
+    this.where({secretTour : {$ne : true}})
+    //regular property can be assigned to the query object too
+    next()
+})
+
+tourSchema.post(`/^find`,function(doc, next){
+    next()
+})
+
+//aggregation middleware
+tourSchema.pre('aggregate',function(next){
+    //this refers to the aggregation object
+    //unshift to add a new object at the beginning
+    this.pipeline().unshift({$match : {price : {$gte : 2000}}})
+    next()
+})
+
 
 //created model
 const Tour = mongoose.model('Tour',tourSchema)
