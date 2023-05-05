cd50c01ff78d6a5aeb6980c3aefb350e105e4222
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-05-05 19:10:11 +0530

    Update tourRoute.js

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const Router = express.Router()
 const Controller = require('../controllers/tourController')
-
+const AuthController = require('../controllers/authController')
 //middleware to check whether the params is valid. instead of writing a condition in every controller
 //we can have a specific middleware which checks it
 // Router.param('id',Controller.checkID)
 
 Router.route('/top5tour').get(Controller.aliasTop5Tour , Controller.getTours)
-Router.route('/').post( Controller.addTour).get(Controller.getTours)
+Router.route('/').post( Controller.addTour).get(AuthController.protect, Controller.getTours)
 Router.route('/tourStats').get(Controller.tourStats)
 Router.route('/getTourByMonth/:year?').get(Controller.getTourByMonth)
-Router.route('/:id').get(Controller.getTourById).patch(Controller.updateTourById).delete(Controller.deleteTourById)
+Router.route('/:id').get(Controller.getTourById).patch(Controller.updateTourById).delete(AuthController.protect, AuthController.ristrict, Controller.deleteTourById)
 
 
 
