a21bd3510b2778846b6a87222b5fde69ac5953ae
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-05-05 19:12:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+
+//error handling for sync code
+process.on("uncaughtException",(err)=> {
+    console.log(`Name : ${err.name} and message : ${err.message} in uncaught exception!!!`)
+     process.exit(1)
+       //0 for success and 1 for exception
+})
+
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path : "./config.env"})
@@ -10,6 +18,17 @@ mongoose.connect(DB).then(()=> console.log("connected to the DB"))
 
 
 const port = process.env.PORT || 3000
-app.listen(port,()=>{
+const server = app.listen(port,()=>{
     console.log(`server running on port : ${port} `)
 })
+
+//if there is an unhandled promise rejection process emits an unhandledRejection object
+//we can subscribe to the event :
+
+process.on('unhandledRejection',(err)=> {
+    console.log(`Name : ${err.name} and message : ${err.message} in unhandledRejection!!!`)
+    //this will execute the task still pending or are being executed
+    server.close(()=>  process.exit(1))
+       //0 for success and 1 for uncalled exception
+})
+
