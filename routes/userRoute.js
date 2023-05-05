a17b7ba9754d15c444d707fa865ffbc3119662e4
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-05-05 19:10:32 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const Router = express.Router()
 const Controller = require('../controllers/userController')
+const AuthController = require('../controllers/authController')
+
+Router.post('/signup',AuthController.signup )
+Router.post('/login',AuthController.login )
 
 
 Router.route('/').get(Controller.getUsers)
