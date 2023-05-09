a5836796f3b76f31133abf8ba43cbbcb58961f00
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-05-09 19:37:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const {appError} = require('./utils/appError')
 app.use(express.json())
 const tourRouter = require('./routes/tourRoute')
 const userRouter = require('./routes/userRoute')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp')
+//to add necessary headers in response
+app.use(helmet())
 
-//logger 3rd party middleware to log the 
+
+//nosql injection attack prevention middleware ( sending query instead of value) - removes . and $
+app.use(mongoSanitize())
+
+//cross site scripting prevent middleware (sending js in html code)
+app.use(xss())
+
+//http parameter pollution prevention
+app.use(hpp())
+
+//logger 3rd party middleware to log  
 if(process.env.NODE_ENV === "development"){
 app.use(morgan('common',
 {
@@ -26,6 +43,15 @@ app.use(morgan('common',
 }
 
 app.use(express.static(`${__dirname}/public`))
+
+
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	message : "too many request from the same IP"
+}) 
+//
+app.use('/v1', apiLimiter)
 
 app.use('/v1/tours',tourRouter)
 app.use('/v1/users',userRouter)
